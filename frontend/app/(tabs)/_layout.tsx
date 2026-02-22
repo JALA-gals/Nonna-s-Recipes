77498a32b629502150e22a674b87a2d031b17e9f
Allie Tivvis <allisontivvis@ufl.edu>
2026-02-22 04:04:44 -0500

    tabs
    tabs

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,14 +1,25 @@
 import { Tabs } from "expo-router";
 import React from "react";
 import CustomTabBar from "../../components/CustomTabBar";
+
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false} } tabBar={(props)=><CustomTabBar{ ... props}/>}>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+    <Tabs
+      screenOptions={{ headerShown: false }}
+      tabBar={(props) => <CustomTabBar {...props} />}
+    >
+      <Tabs.Screen name="homepage" options={{ title: "Home" }} />
+      <Tabs.Screen name="explore" options={{ title: "Explore" }} />
+      <Tabs.Screen name="addrecipes" options={{ title: "Add" }} />
       <Tabs.Screen name="map" options={{ title: "Map" }} />
-      <Tabs.Screen name="recording" options={{ title: "Record" }} />
-      <Tabs.Screen name="App" options={{ title: "Explore" }} />
+      <Tabs.Screen name="UserRecipes" options={{ title: "Me" }} />
+
+      {/* Hide everything else in (tabs) so it won’t appear */}
+      <Tabs.Screen name="index" options={{ href: null }} />
+      <Tabs.Screen name="recording" options={{ href: null }} />
+      <Tabs.Screen name="MyRecipe" options={{ href: null }} />
+      <Tabs.Screen name="settings-test" options={{ href: null }} />
+      <Tabs.Screen name="test-upload" options={{ href: null }} />
     </Tabs>
   );
 }
-
